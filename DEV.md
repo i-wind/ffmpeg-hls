39db164b2670fbf4ff5a6820df3cc144a7268cbb
i.vetrov <ig-wind@mail.ru>
2016-08-06 14:29:08 +0300

    Simple ffmpeg hls streaming

--- DEV.md
+++ DEV.md
@@ -1,10 +1,14 @@
 ## Setup
 
+Prepare virtual environment
+
     $ virtualenv --no-site-packages ~/.pyenv/ffmpeg
     $ source ~/.pyenv/ffmpeg/bin/activate
     $ pip install setuptools --upgrade
     $ pip install pip --upgrade
     $ pip install fabric
     $ pip freeze >require.txt
+
+Install development packages
 
     $ pip install -r require.txt
---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# simple ffmpeg hls streaming
+#
+# usage:
+#   $ python -m SimpleHTTPServer 8000
+#
+#   $ ffplay http://0.0.0.0:8000/hls/playlist.m3u8
+
+NOARGS=65
+
+function usage() {
+    echo "description"
+    echo "  usage: ./start.sh <source>"
+    exit $NOARGS
+}
+
+[[ $# -eq 0 ]] && usage
+
+[ -d hls ] || mkdir hls
+
+SOURCE=$1
+
+ffmpeg -re -i $SOURCE -vf "scale=640:-1" -acodec libfaac -b:a 128k \
+    -c:v libx264 -b:v 750k -flags -global_header -f segment -segment_time 10 \
+    -segment_list hls/playlist.m3u8 -segment_format mpegts hls/playlist_%05d.ts
