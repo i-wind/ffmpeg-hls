ecc25ed33c3428037e9b1b2456167dfab3c29efb
i.vetrov <ig-wind@mail.ru>
2016-08-06 20:17:47 +0300

    Add force key frames parameter

--- multibitrate.sh
+++ multibitrate.sh
@@ -29,12 +29,17 @@ http://0.0.0.0:8000/hls/index1.m3u8
 http://0.0.0.0:8000/hls/index2.m3u8
 EOF
 
-HLS="-hls_time 4 -hls_list_size 8 -hls_wrap 9 -hls_flags delete_segments -start_number 1"
+# HLS="-force_key_frames 0:04:00 -hls_time 4 -hls_list_size 8 -hls_wrap 9 -hls_flags delete_segments -start_number 1"
+HLS="-hls_time 4 -hls_list_size 8 -hls_wrap 8 -hls_flags delete_segments -start_number 1"
 
-ffmpeg -v verbose -i $SOURCE -vcodec libx264 -acodec aac -ac 2 -strict -2 \
-    -vf "[in]yadif=0:-1:0,scale=426:-1[out]" -crf 18 -profile:v baseline \
+ffmpeg -v verbose -i $SOURCE \
+    -vcodec libx264 -acodec aac -ac 2 -strict -2 \
+    -vf "[in]yadif=0:-1:0,scale=426:340[out]" -crf 18 -profile:v baseline \
+    -r 25 -g 100 -force_key_frames "expr:gte(t,n_forced)" \
     -maxrate 400k -bufsize 800k -pix_fmt yuv420p -flags -global_header \
-    -threads 2 $HLS hls/index1.m3u8 -vcodec libx264 -acodec aac -ac 2 \
-    -strict -2 -vf "[in]yadif=0:-1:0,scale=640:-1[out]" -crf 18 \
-    -profile:v baseline -maxrate 750k -bufsize 1500k -pix_fmt yuv420p \
-    -flags -global_header -threads 4 $HLS hls/index2.m3u8
+    -threads 2 $HLS hls/index1.m3u8 \
+    -vcodec libx264 -acodec aac -ac 2 -strict -2 \
+    -vf "[in]yadif=0:-1:0,scale=640:-1[out]" -crf 18 -profile:v baseline \
+    -r 25 -g 100 -force_key_frames "expr:gte(t,n_forced)" \
+    -maxrate 750k -bufsize 1500k -pix_fmt yuv420p -flags -global_header \
+    -threads 4 $HLS hls/index2.m3u8
