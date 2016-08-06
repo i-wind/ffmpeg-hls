09765b004abdef94c7ae498784eb26044a18fc51
i.vetrov <ig-wind@mail.ru>
2016-08-06 18:52:01 +0300

    Standard width for 16:9

--- multibitrate.sh
+++ multibitrate.sh
@@ -32,7 +32,7 @@ EOF
 HLS="-hls_time 4 -hls_list_size 8 -hls_wrap 9 -hls_flags delete_segments -start_number 1"
 
 ffmpeg -v verbose -i $SOURCE -vcodec libx264 -acodec aac -ac 2 -strict -2 \
-    -vf "[in]yadif=0:-1:0,scale=428:-1[out]" -crf 18 -profile:v baseline \
+    -vf "[in]yadif=0:-1:0,scale=426:-1[out]" -crf 18 -profile:v baseline \
     -maxrate 400k -bufsize 800k -pix_fmt yuv420p -flags -global_header \
     -threads 2 $HLS hls/index1.m3u8 -vcodec libx264 -acodec aac -ac 2 \
     -strict -2 -vf "[in]yadif=0:-1:0,scale=640:-1[out]" -crf 18 \
